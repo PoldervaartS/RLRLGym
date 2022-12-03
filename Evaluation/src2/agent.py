29abb35ea9b191b09bdd56abc38d5838f05e7b0d
Tao Jiang <entao@tamu.edu>
2022-12-03 00:16:55 -0600

    evaluation method 2

---
+++ Evaluation/src2/agent.py
@@ -0,0 +1,32 @@
+import os
+import sys
+from stable_baselines3 import PPO
+import pathlib
+from rlgym.utils.action_parsers.discrete_act import DiscreteAction
+
+
+class Agent:
+    def __init__(self):
+        custom_objects = {
+            "lr_schedule": 0.00001,
+            "clip_range": 0.02,
+            "n_envs": 1,
+            "device": "cpu"
+        }
+
+        self.actor = PPO.load("E:\Code\RLRLGym\type1_policy\rl_model_104000000_steps.zip", custom_objects = custom_objects)
+        self.parser = DiscreteAction()
+        pass
+
+    def get_discrete(self, index, val):
+        if index < 5:
+            return val - 1
+        else:
+            return val
+
+    def act(self, state):
+        # Evaluate your model here
+        action = self.actor.predict(state, deterministic = True)
+        x = self.parser.parse_actions(action[0], state)
+
+        return x[0]
